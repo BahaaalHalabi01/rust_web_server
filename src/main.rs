03123dda2fb5dee0f492d343632ec579995b95ff
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-05-14 17:48:48 +0300

    handle 404 and 200 html page

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
-use std::net::{TcpListener,TcpStream};
+use std::fs;
 use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    let listener =TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
-
     }
 }
-fn handle_connection(mut stream:TcpStream){
-    let mut buffer = [0;1024];
+fn handle_connection(mut stream: TcpStream) {
+    // A response looks something like this
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+    //
+    // ex: HTTP/1.1 200 OK\r\n\r\n
+    let http_200: (String, String) = (String::from("HTTP/1.1 200 OK"), String::from("index.html"));
+    let http_404: (String, String) = (
+        String::from("HTTP/1.1 404 NOT FOUND"),
+        String::from("404.html"),
+    );
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        http_200
+    } else {
+        http_404
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
-
-// A response looks something like this
-// HTTP-Version Status-Code Reason-Phrase CRLF
-// headers CRLF
-// message-body
-//
-// ex: HTTP/1.1 200 OK\r\n\r\n
