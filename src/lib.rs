27f522826e44b38c73965bf54805f2ee9d5bc50f
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-05-14 23:15:25 +0300

    terminate message

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-use std::{thread, sync::{mpsc::{self, Receiver}, Arc, Mutex}};
+use std::{
+    sync::{
+        mpsc::{self, Receiver},
+        Arc, Mutex,
+    },
+    thread,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender:mpsc::Sender<Job>
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -14,41 +20,70 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let (sender,reciever) = mpsc::channel();
+        let (sender, reciever) = mpsc::channel();
 
         let reciever = Arc::new(Mutex::new(reciever));
-        let mut workers= Vec::with_capacity(size);
-        for id  in 0..size {
-            workers.push(Worker::new(id,Arc::clone(&reciever)))
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&reciever)))
         }
 
-        ThreadPool {workers,sender}
+        ThreadPool { workers, sender }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap()
+            }
+        }
     }
 }
 
 struct Worker {
-    id:usize,
-    thread:thread::JoinHandle<()>
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id:usize,reciever:Arc<Mutex<Receiver<Job>>>)-> Self {
+    fn new(id: usize, reciever: Arc<Mutex<Receiver<Message>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            let job = reciever.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.",id);
+            let message = reciever.lock().unwrap().recv().unwrap();
 
-            job();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
         });
-        Worker { id,thread}
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
-
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
