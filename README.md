c4e58be226ded12c2bd4817681f66dffad60db4e
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2024-01-05 20:58:19 +0300

    add readme, add sleep route for test

---
+++ README.md
@@ -0,0 +1,7 @@
+## About Project
+- This is a learning project for `rust` which allows me to create a multi-threaded `http web server`
+
+
+## Running the app
+
+- run `cargo run` and navigate to `localhost:7878` in your browser
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,14 +7,11 @@ use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    
+
     let pool = ThreadPool::new(4);
     for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        pool.execute(||{
-            handle_connection(stream)
-        })
-        
+        pool.execute(|| handle_connection(stream))
     }
 }
 fn handle_connection(mut stream: TcpStream) {
@@ -24,22 +21,22 @@ fn handle_connection(mut stream: TcpStream) {
     // message-body
     //
     // ex: HTTP/1.1 200 OK\r\n\r\n
-    let route_home =
-        ("HTTP/1.1 200 OK","index.html");
-    let route_post = ("HTTP/1.1 200 OK","post.html");
-    let route_404  = (
-        "HTTP/1.1 404 NOT FOUND",
-        "404.html",
-    );
+    let route_home = ("HTTP/1.1 200 OK", "index.html");
+    let route_post = ("HTTP/1.1 200 OK", "post.html");
+    let route_404 = ("HTTP/1.1 404 NOT FOUND", "404.html");
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     let get_post = b"GET /post HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         route_home
     } else if buffer.starts_with(get_post) {
+        thread::sleep(Duration::from_secs(15));
+        route_home
+    } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
         route_post
     } else {
